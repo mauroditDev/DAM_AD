f70cbb1a558e627d072f2ae10bad7a8f573c3a31
root <root@debian>
2016-10-07 10:08:55 +0200

    agregada funcion recursiva para buscar ficheros segun criterio en Ud1Prac2

--- Workspace_PrimeraUd/Ud1_Prac2/src/impresionCarpetas/ImpresionCarpeta.java
+++ Workspace_PrimeraUd/Ud1_Prac2/src/impresionCarpetas/ImpresionCarpeta.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class ImpresionCarpeta {
 
+	/*
+	 * función main para el listado de archivos y carpetas en arbol
+	 * 
 	public static void main(String[] args) {
 		//String para almacenar la ruta del archivo
 		String ruta;
@@ -14,10 +17,6 @@ public class ImpresionCarpeta {
 		if(args.length > 0)
 			ruta = args[0]; //si se dieron se trata el primero como la ruta
 		else{
-			/*
-			 * si no se dieron argumentos se requiere el ingreso de una ruta
-			 * en forma de String
-			 */
 			Scanner sc = new Scanner(System.in);
 			System.out.println("introduzca ruta de la carpeta:");
 			ruta = sc.next();
@@ -27,6 +26,51 @@ public class ImpresionCarpeta {
 		
 		File archivo = new File(ruta);
 		mostrarDirectorio(archivo,espacios);
+		
+
+	}
+	*/
+	
+	/*
+	 *función main para la búsqueda de los archivos según un criterio 
+	 *
+	 */
+	public static void main(String[] args) {
+		//String para almacenar la ruta del archivo
+		String ruta;
+		String criterio;
+		char crit;
+		Long tamano;
+		int espacios = 0;
+		//compruba si se dieron argumentos en la ejecución
+		if(args.length > 0){
+			ruta = args[0]; //si se dieron se trata el primero como la ruta
+			crit = args[1].charAt(0);
+			tamano = Long.parseLong(args[2]);
+		}
+		else{
+			/*
+			 * si no se dieron argumentos se requiere el ingreso de una ruta
+			 * en forma de String
+			 */
+			Scanner sc = new Scanner(System.in);
+			System.out.println("introduzca ruta de la carpeta:");
+			ruta = sc.next();
+			System.out.println("criterio de busqueda '+', '-' o '=': ");
+			criterio = sc.next();
+			crit = criterio.charAt(0);
+			System.out.println("itamaño de la búsqueda:");
+			tamano = Long.parseLong(sc.next());
+			sc.close();
+		}
+		
+		
+		File archivo = new File(ruta);
+		ArrayList<File> resultado = buscarArchivoPorTamano(archivo,crit,tamano);
+		for(int i = 0; i < resultado.size(); i++){
+			System.out.println(resultado.get(i).getAbsolutePath().replaceAll(ruta, "")+ " ---- "+resultado.get(i).length());
+		}
+		System.out.println();
 		
 
 	}
@@ -72,12 +116,39 @@ public class ImpresionCarpeta {
 	}
 	
 	public static ArrayList<File> buscarArchivoPorTamano(File rutaComienzo, char criterio, long tamano){
+		
 		ArrayList<File> res = new ArrayList<File>();
 		
-		//TODO: función que se come un criterio ('+' para mayor que, '-' para menor que),
-		//y según este criterio busca todos los archivos de tamaño dado, los almacena en ArrayList
-		//y devuelve.
+		File[] lista = null;
+		if(rutaComienzo.exists() && rutaComienzo.isDirectory())
+			 lista = rutaComienzo.listFiles();
 		
+		if(lista!=null){
+			
+			for(int i = 0; i< lista.length; i++){
+				if(lista[i].isDirectory() && !lista[i].isHidden()){
+					res.addAll(buscarArchivoPorTamano(lista[i],criterio,tamano));
+				}
+				else{
+					if(!lista[i].isHidden()){
+						switch(criterio){
+						case '+':
+							if(lista[i].length() > tamano)
+								res.add(lista[i]);
+							break;
+						case '-':
+							if(lista[i].length() < tamano)
+								res.add(lista[i]);
+							break;
+						case '=':
+							if(lista[i].length() == tamano)
+								res.add(lista[i]);
+							break;
+						}
+					}	
+				}
+			}
+		}
 		
 		return res;
 	}
