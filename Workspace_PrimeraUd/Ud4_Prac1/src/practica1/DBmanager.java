24216525866b3045b474a6a6630bf4e1549b896d
Alumno <alumno@debian>
2016-11-25 12:43:48 +0100

    25-11-201 6

--- Workspace_PrimeraUd/Ud4_Prac1/src/practica1/DBmanager.java
+++ Workspace_PrimeraUd/Ud4_Prac1/src/practica1/DBmanager.java
@@ -1,9 +1,13 @@
 package practica1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 
 
 
@@ -13,26 +17,85 @@ public class DBmanager {
     public Statement stmnt;
     
     public DBmanager(){
-           try {
+       try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			con = DriverManager.getConnection("jdbc:mysql://localhost/banco","root","root");
+			stmnt = con.createStatement();
+		} catch(MySQLSyntaxErrorException ex){
+			try {
+				con = DriverManager.getConnection("jdbc:mysql://localhost/","root","root");
+				stmnt = con.createStatement();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-           
-           try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost/almacen",
-			           "root","root");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-           
-          
-           
-
-         
+		} 
     }
 
+    public boolean createTables() throws YaExisteException{
+    	boolean res = true;
+    	try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/","root","root");
+			stmnt = con.createStatement();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+    	try{
+    		BufferedReader br = new BufferedReader(new FileReader("/media/Datos/Datos_Mauro/Acceso_Datos/Workspace_PrimeraUd/Ud4_Prac1/banco.sql"));
+    		String sentencia = "";
+    		String linea = br.readLine();
+    		while(linea!=null){
+    			sentencia += linea + " ";
+    			linea = br.readLine();
+    		}
+    		br.close();
+    		String[] sentencias = sentencia.split("\\\\");
+    		for (String sent: sentencias){
+    			sent.replaceAll("\\\\", "");
+    			if(!sent.equals(""))
+    				stmnt.executeUpdate(sent);
+    		}
+    		
+    	}catch(SQLException e){
+    		throw new YaExisteException("La base de datos ya existe");
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		res = false;
+    	}
+    	try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/banco","root","root");
+			stmnt = con.createStatement();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+    	return res;
+    	
+    }
+
+	public boolean reiniciarTablas() {
+		boolean res = true;
+		try{
+			stmnt.executeUpdate("drop database banco");
+			createTables();
+		}catch(Exception e){
+			res = false;
+		}
+		return res;
+	}
+
+	public boolean addCliente(String nombre, String fecha, String direccion) {
+		boolean res = true;
+		try{
+			stmnt.executeUpdate(
+					"INSERT INTO cliente (nombre,f_nac,direccion) VALUES"
+					+ "( '"+nombre+"', "
+					+ " '"+fecha+"', "
+					+ "'"+direccion+"')"
+					);
+		}catch(Exception e){
+			res = false;
+		}
+		return res;
+	}
 }
---
+++ Workspace_PrimeraUd/Ud4_Prac1/src/practica1/YaExisteException.java
@@ -0,0 +1,13 @@
+package practica1;
+
+public class YaExisteException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public YaExisteException(String mensaje){
+		super();
+	}
+
+}
